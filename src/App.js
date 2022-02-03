8e31b3611e70bb8441a02808866c65dba49505e5
Alexis Mora <alexis.mora1v@gmail.com>
2022-02-03 01:55:04 -0600

    Create state and import component
    Create an state to store the data from the products.
    Import the component Producto with two props.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import Productos from './components/Productos'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    productos: [
+      {name: 'Tomate', price: 27, img: '/media/productos/tomate.jpg'},
+      {name: 'Cebolla', price: 27, img: '/media/productos/cebolla.jpg'},
+      {name: 'Papa', price: 27, img: '/media/productos/papa.jpg'},
+      {name: 'Zanahoria', price: 27, img: '/media/productos/zanahoria.png'},
+      {name: 'Limon', price: 27, img: '/media/productos/limon.jpg'},
+      {name: 'Chicharos', price: 27, img: '/media/productos/arbejas.jpg'},
+      {name: 'Lechuga', price: 27, img: '/media/productos/lechuga.jpg'}
+    ]
+  }
+
+  render() {
+    return (
+      <div>
+        <Productos
+          agregarAlCarrito={() => console.log('No hace nada')}
+          productos={this.state.productos}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
