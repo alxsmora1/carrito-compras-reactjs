0a9f7d4556a17a3375d680441b1971e801dd700e
Alexis Mora <alexis.mora1v@gmail.com>
2022-02-03 23:50:34 -0600

    Import navbar and title

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import { Component } from 'react';
-import Productos from './components/Productos'
+import { Component } from "react";
+import Productos from "./components/Productos";
+import Layout from "./components/Layout";
+import Title from "./components/Title";
+import Navbar from "./components/Navbar";
 
 class App extends Component {
   state = {
     productos: [
-      {name: 'Tomate', price: 27, img: '/media/productos/tomate.jpg'},
-      {name: 'Cebolla', price: 27, img: '/media/productos/cebolla.jpg'},
-      {name: 'Papa', price: 27, img: '/media/productos/papa.jpg'},
-      {name: 'Zanahoria', price: 27, img: '/media/productos/zanahoria.png'},
-      {name: 'Limon', price: 27, img: '/media/productos/limon.jpg'},
-      {name: 'Chicharos', price: 27, img: '/media/productos/arbejas.jpg'},
-      {name: 'Lechuga', price: 27, img: '/media/productos/lechuga.jpg'}
-    ]
-  }
+      { name: "Tomate", price: 27, img: "/media/productos/tomate.jpg" },
+      { name: "Cebolla", price: 27, img: "/media/productos/cebolla.jpg" },
+      { name: "Papa", price: 27, img: "/media/productos/papa.jpg" },
+      { name: "Zanahoria", price: 27, img: "/media/productos/zanahoria.png" },
+      { name: "Limon", price: 27, img: "/media/productos/limon.jpg" },
+      { name: "Chicharos", price: 27, img: "/media/productos/arbejas.jpg" },
+      { name: "Lechuga", price: 27, img: "/media/productos/lechuga.jpg" },
+    ],
+  };
 
   render() {
     return (
       <div>
-        <Productos
-          agregarAlCarrito={() => console.log('No hace nada')}
-          productos={this.state.productos}
-        />
+        <Navbar />
+        <Layout>
+          <Title />
+          <Productos
+            agregarAlCarrito={() => console.log("No hace nada")}
+            productos={this.state.productos}
+          />
+        </Layout>
       </div>
     );
   }
