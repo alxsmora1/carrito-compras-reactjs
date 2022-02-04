c897fbca2430493805c652fdd2c7962067e67dbf
Alexis Mora <alexis.mora1v@gmail.com>
2022-02-03 23:51:55 -0600

    Create a Navbar component
    Navbar component for a logo and shopping car components

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { Component } from 'react';
+import Logo from './Logo';
+import Carrito from './Carrito';
+
+const styles = {
+    navbar: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        height: '100px',
+        justifyContent: 'space-between',
+        position: 'relative',
+        padding: '0 50px',
+        boxShadow: '0 2px 3px 0 rgb(0, 0, 0, 0.1)',
+    }
+}
+class Navbar extends Component {
+  render() {
+    return (
+        <nav style={styles.navbar}>
+            <Logo />
+            <Carrito />
+        </nav>
+    );
+  }
+}
+
+export default Navbar;
